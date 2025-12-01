804ba32c544e1984fdca9e19972c54c6191eb2cf
Gabriela Sena da Silva <44529047+ssilvagabriela@users.noreply.github.com>
2025-12-01 15:37:42 -0300

    Add files via upload

---
+++ PR_SGV_CARGA_RESUMO_OCORR_SAC.sql
@@ -0,0 +1,121 @@
+create or replace procedure PR_SGV_CARGA_RESUMO_OCORR_SAC
+
+is
+
+cursor c_abertura_sac is
+    select 	s.nr_sac,
+            s.dt_abertura_sac,
+            s.hr_abertura_sac,
+            s.tp_sac,
+            s.nr_indice_satisfacao,
+            s.cd_produto,
+            p.ds_produto,
+            p.vl_unitario vl_unitario_prod,
+            p.vl_perc_lucro,
+            s.nr_cliente,
+            c.nm_cliente
+    from	mc_sgv_sac s 
+    inner join mc_produto p on (s.cd_produto = p.cd_produto)
+    join mc_cliente c on (s.nr_cliente = c.nr_cliente);
+
+r_cursor_sac                    c_abertura_sac%rowtype;
+
+v_ds_tipo_classificacao_sac 	mc_sgv_ocorrencia_sac.ds_tipo_classificacao_sac%type;
+v_ds_indice_satisfacao_atd_sac 	mc_sgv_ocorrencia_sac.ds_indice_satisfacao_atd_sac%type;
+v_vl_unitario_lucro_produto     mc_sgv_ocorrencia_sac.vl_unitario_lucro_produto%type;
+v_vl_icms_produto               mc_sgv_ocorrencia_sac.vl_icms_produto%type;
+v_sg_estado                     mc_estado.sg_estado%type;
+v_nm_estado                     mc_estado.nm_estado%type;
+v_nr_ocorrencia_sac             number;
+
+begin
+
+	execute immediate 'truncate table mc_sgv_ocorrencia_sac';
+
+	open c_abertura_sac;
+	loop
+		fetch c_abertura_sac into r_cursor_sac;
+
+		if c_abertura_sac%notfound then
+			exit;
+		end if;
+
+		if r_cursor_sac.tp_sac = 'D' then
+			v_ds_tipo_classificacao_sac := 'DUVIDA';
+		elsif r_cursor_sac.tp_sac = 'S' then
+			v_ds_tipo_classificacao_sac := 'SUGESTAO';
+		end if;
+
+        prc_mc_gera_indice_satisfacao_atd(r_cursor_sac.nr_indice_satisfacao, v_ds_indice_satisfacao_atd_sac);
+
+        v_vl_unitario_lucro_produto :=  r_cursor_sac.vl_unitario_prod - (r_cursor_sac.vl_unitario_prod * (r_cursor_sac.vl_perc_lucro/100));
+
+        begin
+            select 	cid.sg_estado, est.nm_estado
+            into	v_sg_estado, v_nm_estado
+            from mc_estado est inner join mc_cidade cid 
+            on (cid.sg_estado = est.sg_estado) inner join mc_bairro b
+            on (b.cd_cidade = cid.cd_cidade) inner join mc_logradouro l
+            on (l.cd_bairro = b.cd_bairro) inner join mc_end_cli ec
+            on (l.cd_logradouro = ec.cd_logradouro_cli)
+            where ec.nr_cliente = r_cursor_sac.nr_cliente and rownum < 2;
+        exception
+            when no_data_found then
+                v_sg_estado := '';
+                v_nm_estado := '';
+        end;
+
+        v_vl_icms_produto := fun_mc_gera_aliquota_media_icms_estado(v_sg_estado);
+
+        select  nvl(max(nr_ocorrencia_sac),0)+1
+        into    v_nr_ocorrencia_sac
+        from    mc_sgv_ocorrencia_sac;
+
+        begin
+            insert into mc_sgv_ocorrencia_sac(
+            nr_ocorrencia_sac, 
+            dt_abertura_sac, 
+            hr_abertura_sac, 
+            ds_tipo_classificacao_sac,
+            ds_indice_satisfacao_atd_sac, 
+            cd_produto, 
+            ds_produto, 
+            vl_unitario_produto, 
+            vl_perc_lucro, 
+            vl_unitario_lucro_produto, 
+            sg_estado, 
+            nm_estado,
+            nr_cliente, 
+            nm_cliente, 
+            vl_icms_produto)
+            values
+            (
+            v_nr_ocorrencia_sac,
+            r_cursor_sac.dt_abertura_sac,
+            r_cursor_sac.hr_abertura_sac,
+            v_ds_tipo_classificacao_sac,
+            v_ds_indice_satisfacao_atd_sac,
+            r_cursor_sac.cd_produto,
+            r_cursor_sac.ds_produto,
+            r_cursor_sac.vl_unitario_prod,
+            r_cursor_sac.vl_perc_lucro,
+            v_vl_unitario_lucro_produto,
+            v_sg_estado,
+            v_nm_estado,
+            r_cursor_sac.nr_cliente,
+            r_cursor_sac.nm_cliente,
+            v_vl_icms_produto            
+            );
+        exception
+            when others then
+                raise_application_error( -20222, 'Erro crítico !' || SQLErrM);
+        end;
+
+	end loop;
+
+	commit;
+    close c_abertura_sac;
+
+	dbms_output.put_line('Processamento concluído com sucesso!' );
+
+end;
